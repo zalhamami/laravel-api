ec282d4e5e8525927ec84117c8dd7593bdb39a99
Farizal Hamami <farizalhamami@gmail.com>
2020-12-14 21:39:01 +0700

    Create storage service

---
+++ app/Services/StorageService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Storage;
+use phpDocumentor\Reflection\File;
+
+class StorageService {
+    /**
+     * @var \Illuminate\Contracts\Filesystem\Filesystem
+     */
+    private $storage;
+
+    /**
+     * @var bool
+     */
+    private $fromString;
+
+    /**
+     * StorageService constructor.
+     * @param string $storageType
+     * @param bool $fromString
+     */
+    public function __construct(string $storageType = 'local', bool $fromString = false)
+    {
+        $this->storage = Storage::disk($storageType);
+        $this->fromString = $fromString;
+    }
+
+    /**
+     * @param string $path
+     * @param $file
+     * @param string $permission
+     * @return mixed
+     */
+    public function save(string $path, $file, string $permission = 'public')
+    {
+        if (!$this->fromString) {
+            $file = file_get_contents($file);
+        }
+        $this->storage->put($path, $file, $permission);
+        return $this->storage->url($path);
+    }
+
+    /**
+     * @param string $path
+     */
+    public function delete(string $path)
+    {
+        $this->storage->delete($path);
+    }
+}
