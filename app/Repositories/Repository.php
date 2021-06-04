30f698796f61af0de1328d2dbbd7402cf621d6a6
Farizal Hamami <farizalhamami@gmail.com>
2021-06-04 15:03:50 +0700

    Add some functions in repository

--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -25,6 +25,7 @@ class Repository
         if ($query) {
             $collection = $query;
         }
+
         if ($request['sort_by']) {
             if ($request['sort_direction'] == 'desc') {
                 $collection = $collection->orderByDesc($request['sort_by']);
@@ -32,9 +33,29 @@ class Repository
                 $collection = $collection->orderBy($request['sort_by']);
             }
         }
-        $data = $collection->paginate($request['per_page']);
+        if ($request['keyword']) {
+            $collection = $collection->where('name', 'like', '%' . $request['keyword'] . '%');
+        }
 
+        $data = $collection->paginate($request['per_page']);
         return $data;
+    }
+
+    public function getAllWithFilters(array $filters = [], $or = false)
+    {
+        $query = $this->model;
+        for ($i = 0; $i < count($filters); $i++) {
+            if ($i === 0) {
+                $query = $query->where($filters[$i]['field'], $filters[$i]['value']);
+            } else {
+                if ($or === true) {
+                    $query = $query->orWhere($filters[$i]['field'], $filters[$i]['value']);
+                } else {
+                    $query = $query->where($filters[$i]['field'], $filters[$i]['value']);
+                }
+            }
+        }
+        return $this->getAll($query);
     }
 
     public function getAllWithDetails()
@@ -52,6 +73,18 @@ class Repository
         return $data;
     }
 
+    public function getByField($field, $value, $comparator = '=', $latest = false)
+    {
+        if (!$this->model) {
+            return null;
+        }
+        $data = $this->model->details()->where($field, $comparator, $value);
+        if ($latest) {
+            $data = $data->orderByDesc('id');
+        }
+        return $data->firstOrFail();
+    }
+
     public function create($payload)
     {
         return $this->model->create($payload);
@@ -62,5 +95,24 @@ class Repository
         $data = $this->getById($id);
         $data->update($payload);
         return $data;
+    }
+
+    public function delete($id)
+    {
+        $data = $this->getById($id);
+        $data->delete();
+        return $data;
+    }
+
+    public function forceDelete($id)
+    {
+        $data = $this->getById($id);
+        $data->forceDelete();
+        return $data;
+    }
+
+    public function restore(int $id)
+    {
+        return $this->model->withTrashed()->findOrFail($id)->restore();
     }
 }
