60b6bf7b2aca8ed5931f3165af8c4e8132399dae
Farizal Hamami <farizalhamami@gmail.com>
2021-06-10 13:21:13 +0700

    Fix reset password page

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -13,7 +13,7 @@
             @csrf
 
             <!-- Password Reset Token -->
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                <input type="hidden" name="token" value="{{ $request['token'] }}">
 
             <!-- Email Address -->
             <div>
